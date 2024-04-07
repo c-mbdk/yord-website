3664fe960904fb691a92e51f4073320bdba54f4c
Chisom Mgbedike <you@example.com>
2024-04-07 23:51:33 +0100

    adding new test_results folder

--- yord_website/__init__.py
+++ yord_website/__init__.py
@@ -139,7 +139,6 @@ def register_cli_commands(app):
     @app.cli.command()
     def test():
         """Runs all tests."""
-        os.mkdir(test_results)
         pytest.main(["-s", "--cov=yord_website", "--junit-xml=test_results/junit.xml", 'tests'])
         echo('All tests have been run and an XML report produced.')
 
