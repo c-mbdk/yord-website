5e434c92388d5f2f1c0c019b95d2b17472aa6e28
Chisom Mgbedike <you@example.com>
2024-04-08 17:58:13 +0100

    testing use of exit in flask cli function

--- yord_website/__init__.py
+++ yord_website/__init__.py
@@ -128,7 +128,7 @@ def register_cli_commands(app):
     def test():
         """Runs all tests."""
         echo('Running all tests and producing an XML report...')
-        pytest.main(["--tb=no", "--minpass=86", "--junit-xml=test_results/junit.xml", 'tests'])
+        exit(pytest.main(["-s", "--minpass=86", "--junit-xml=test_results/junit.xml", 'tests']))
         
 
     @app.cli.command()
