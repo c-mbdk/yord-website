fa62d0eb01515dddb34be6cf63d1260df3c82b59
Chisom Mgbedike <you@example.com>
2024-04-08 14:48:05 +0100

    adding a min expected to pass to force different exit code

--- tests/conftest.py
+++ tests/conftest.py
@@ -86,6 +86,31 @@ def init_empty_database(test_client):
     
     db.drop_all()
 
+def pytest_addoption(parser):
+    parser.addoption('--minpass', type=int, default=0, help='minimum amount of tests to pass')
+
+
+def pytest_sessionstart(session):
+    session.count_passed = 0
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == 'call' and result.passed:
+        item.session.count_passed += 1
+
+
+def pytest_sessionfinish(session, exitstatus):
+    min_passed = session.config.getoption('minpass')
+    if session.count_passed < min_passed:
+        session.exitstatus = 127
+        reporter = session.config.pluginmanager.get_plugin('terminalreporter')
+        reporter.section('Session errors', sep='-', red=True, bold=True)
+        reporter.line(f'Not enough successful tests - expected at least {min_passed} to pass, passed {session.count_passed}')
+
 
 # -------------
 # Test Data
